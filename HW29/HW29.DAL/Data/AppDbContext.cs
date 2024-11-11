56e89a16808acabad84ecc89e536235897ccbd8f
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-11 20:53:01 +0200

    Create AppDbContext.cs

---
+++ HW29/HW29.DAL/Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using HW29.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HW29.DAL.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public virtual DbSet<Car> Cars => Set<Car>();
+		public virtual DbSet<Product> Products => Set<Product>();
+		public virtual DbSet<CarShowroom> CarShowrooms => Set<CarShowroom>();
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer();
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Car>()
+				.HasKey(c => c.Id);
+			modelBuilder.Entity<Product>()
+				.HasKey(p => p.Id);
+			modelBuilder.Entity<CarShowroom>()
+				.HasKey(cs => cs.Id);
+
+			modelBuilder.Entity<Car>()
+				.Property(c => c.Model)
+				.IsRequired();
+
+			modelBuilder.Entity<Car>()
+				.Property(c => c.Brand)
+				.IsRequired();
+			
+			modelBuilder.Entity<Product>()
+				.Property(p => p.Price)
+				.IsRequired();
+
+			modelBuilder.Entity<CarShowroom>()
+				.Property(cs => cs.Name)
+				.IsRequired();
+
+			modelBuilder.Entity<CarShowroom>()
+				.Property(cs => cs.Address)
+				.IsRequired();
+
+
+			modelBuilder.Entity<Product>()
+				.HasOne<Car>(p => p.Car)
+				.WithOne();
+
+			modelBuilder.Entity<CarShowroom>()
+				.HasMany<Product>(cs => cs.Products)
+				.WithOne();
+		}
+	}
+}
