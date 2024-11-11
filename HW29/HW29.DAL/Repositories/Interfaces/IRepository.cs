0ebb0291c52101958365e2249c15b8dca8da86cd
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-11 21:08:03 +0200

    Create generic repository

---
+++ HW29/HW29.DAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace HW29.DAL.Repositories.Interfaces
+{
+	public interface IRepository<T>
+	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+	}
+}
