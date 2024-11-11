87763860945ab34ce71484f16bd03a34e4b3ff58
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-11 20:39:31 +0200

    Create models

---
+++ HW29/HW29.DAL/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace HW29.DAL.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+
+		public string Brand { get; set; } = string.Empty;
+
+		public string Model { get; set; } = string.Empty;
+	}
+}
---
+++ HW29/HW29.DAL/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace HW29.DAL.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		public decimal Price { get; set; }
+
+		public Car Car { get; set; } = null!;
+		public int CarId { get; set; }
+	}
+}
