ca164bd53a771b5c6d2bae45072b72337e54b742
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-11 21:30:41 +0200

    Create product service. Add method for car sale

---
+++ HW29/HW29.BL/Services/Interfaces/IProductService.cs
@@ -0,0 +1,7 @@
+﻿namespace HW29.BL.Services.Interfaces
+{
+	public interface IProductService
+	{
+		Task SellCar(int productId);
+	}
+}
---
+++ HW29/HW29.BL/Services/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using HW29.BL.Services.Interfaces;
+using HW29.DAL.Repositories.Interfaces;
+
+namespace HW29.BL.Services
+{
+	public class ProductService(IProductRepository repository) : IProductService
+	{
+		private readonly IProductRepository _repository = repository;
+
+		public async Task SellCar(int productId)
+		{
+			var product = await _repository.GetByIdAsync(productId);		
+			
+			if (product == null)
+			{
+				throw new Exception("Product not found");
+			}
+
+			await _repository.DeleteAsync(product);
+		}
+	}
+}
