dd579395022939004b548ea7736afed599bb43e8
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-11-12 19:59:45 +0200

    Update controller. Registered services

---
+++ HW29/HW29.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using HW29.BL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW29.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController(IProductService productService) : Controller
+	{
+		private readonly IProductService _productService = productService;
+
+		[HttpDelete]
+		public async Task<IActionResult> SellCar(int productId)
+		{
+			try
+			{
+				await _productService.SellCarAsync(productId);
+
+				return Ok();
+			}
+			catch (Exception)
+			{
+				return NotFound();
+			}
+		}
+	}
+}
--- HW29/HW29.WebAPI/Program.cs
+++ HW29/HW29.WebAPI/Program.cs
@@ -1,3 +1,9 @@
+using HW29.BL.Services;
+using HW29.BL.Services.Interfaces;
+using HW29.DAL.Data;
+using HW29.DAL.Repositories;
+using HW29.DAL.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>();
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICarShowroomRepository, CarShowroomRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
